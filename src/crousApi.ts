a727ca43a2aebdc3bc049ca4a8be1eea0c4a786f
Le-Roux-Nard <guymaxyt@gmail.com>
2022-10-19 14:57:22 +0200

    add failsafe if Xml Files can't be parsed

--- src/crousApi.ts
+++ src/crousApi.ts
@@ -81,7 +81,17 @@ class CrousAPI {
 							url: dataset.url,
 						});
 
-						let parsedResult = JSON.parse(xml2json(data, { compact: true }));
+						let parsedResult;
+						try {
+							parsedResult = JSON.parse(xml2json(data, { compact: true }));
+						} catch (err) {
+							console.error(
+								(err as Error).message == "Attribute without value"
+									? `Erreur lors du parsing XML de ${dataset.title} : le fichier semble être mal formé`
+									: err
+							);
+							continue;
+						}
 						if (isXmlRestaurant(parsedResult)) {
 							let restaurants = parseRestaurantsFromXml(parsedResult);
 							this.listeCrous.get(idCrous)?.restaurants.push(...restaurants);
@@ -127,7 +137,17 @@ class CrousAPI {
 					url: dataset.url,
 				});
 
-				let parsedResult = JSON.parse(xml2json(data, { compact: true }));
+				let parsedResult;
+				try {
+					parsedResult = JSON.parse(xml2json(data, { compact: true }));
+				} catch (err) {
+					console.error(
+						(err as Error).message == "Attribute without value"
+							? `Erreur lors du parsing XML de ${dataset.title} : le fichier semble être mal formé`
+							: err
+					);
+					continue;
+				}
 				if (isXmlMenu(parsedResult)) {
 					let listsOfMenus = parseMenusFromXml(parsedResult).reduce((acc: { id: string; menus: Menu[] }[], menu: Menu) => {
 						const index = acc.findIndex((a) => a.id === menu.id);
