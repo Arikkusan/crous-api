38e897121ee2608431e7080a52b01354f8c648bc
Le-Roux-Nard <guymaxyt@gmail.com>
2023-06-08 16:57:03 +0200

    Update test runner conf

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,13 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transform:{
-    "^.+\\.tsx?$": "ts-jest"
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { "useESM": true }]
   },
+  moduleNameMapper: {
+    "(.+)\\.js": "$1"
+  },
+  resolver: "ts-jest-resolver",
+  extensionsToTreatAsEsm: [".ts"],
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
