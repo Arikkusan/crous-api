1700e6adb729ec6abcf739aa903cbc59bcc325f6
Le-Roux-Nard <guymaxyt@gmail.com>
2023-06-08 16:57:20 +0200

    Added test for publicHolidaysManager and HolidaysManager

--- src/__mocks__/server.ts
+++ src/__mocks__/server.ts
@@ -1,10 +1,14 @@
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 
+import holidaysJSON from "./data/holidays-2023.json"; //assert { type: "json" };
+import publicHolidaysJSON from "./data/publicHolidays-2023.json"; //assert { type: "json" };
+
 // handlers are usually saved in separate file(s) in one  destined place of the app,
 // so that you don't have to search for them when the endpoints have changed
 const handlers = [
-	rest.get(/https\:\/\/calendrier\.api\.gouv\.fr\/jours-feries\/metropole\/.+/, (req, res, ctx) => res(ctx.json({ message: "success" }))),
+	rest.get("https://data.education.gouv.fr/api/records/1.0/search/", (req, res, ctx) => res(ctx.json(holidaysJSON))),
+	rest.get(/https\:\/\/calendrier\.api\.gouv\.fr\/jours-feries\/metropole\/\d{4}/, (req, res, ctx) => res(ctx.json(publicHolidaysJSON))),
 ];
 
 const server = setupServer(...handlers);
--- src/__tests__/crousApi.integration.test.ts
+++ src/__tests__/crousApi.integration.test.ts
@@ -1,11 +1,47 @@
-import { publicHolidaysAxios } from "../utils/AxiosCustom";
+import PublicHolidaysManager from "../utils/publicHolydayManager";
+import HolidaysManager from "../utils/HolidaysManager";
 import server from "../__mocks__/server";
 
-beforeAll(() => server.listen());
+beforeAll(() => {
+	server.listen();
+	jest.useFakeTimers().setSystemTime(new Date("2023-03-01"));
+});
+
+describe("PublicHolidayManager", () => {
+	it("should return Public Holidays List for 2023", async () => {
+		jest.useFakeTimers().setSystemTime(new Date("2023-01-01"));
+
+		const publicHolidaysManager = new PublicHolidaysManager();
+		await publicHolidaysManager.updateCache();
+		expect(publicHolidaysManager.getPublicHolydays()).toHaveLength(11);
+
+		const jourDeLan = publicHolidaysManager.getPublicHolydays()[0];
+		expect(jourDeLan.description).toBe("1er janvier");
+		expect(jourDeLan.date).toStrictEqual(new Date("2023-01-01"));
+		expect(jourDeLan.actual).toBeTruthy();
+	});
+});
 
 describe("HolidayManager", () => {
-	it("should return hello", async () => {
-		await publicHolidaysAxios.get("2023").then((res) => console.log(res.data));
-		expect(true).toBeTruthy();
+	it("should return Holidays List for 2022-2023 school year", async () => {
+		const holidayManager = new HolidaysManager();
+		await holidayManager.updateCache();
+		const holidays = holidayManager.getStandardVacances();
+		expect(holidays.size).toBe(holidays.size);
+	});
+
+	it("should delay start of special holidays", async () => {
+		const holidayManager = new HolidaysManager();
+		await holidayManager.updateCache();
+		await holidayManager.loadCustomVacances();
+		const customHolidays = holidayManager.getCustomVacances();
+		console.log(holidayManager.getStandardVacances());
+
+		expect(customHolidays?.["Le Mans Université"]).toBeDefined();
+
+		const expectedStartDate: Date = new Date("2022-10-28");
+		expectedStartDate.setUTCHours(22);
+
+		expect(customHolidays?.["Le Mans Université"]![0].start_date).toStrictEqual(expectedStartDate);
 	});
 });
