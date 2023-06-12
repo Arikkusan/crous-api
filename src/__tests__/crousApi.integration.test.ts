feedf25265e3829e06f25e13275d9f962a37434b
Le-Roux-Nard <guymaxyt@gmail.com>
2023-06-12 10:27:48 +0200

    remove useless test and add more tests

--- src/__tests__/crousApi.integration.test.ts
+++ src/__tests__/crousApi.integration.test.ts
@@ -21,15 +21,28 @@ beforeAll(
 
 afterAll(() => {
 	interval.unref();
-})
+});
+
+afterEach(() => {
+	jest.useRealTimers();
+});
 
 describe("ResourceManager", () => {
-	it("should have fetch results", async () => {
-		expect(true).toBeTruthy();
-	});
-
 	it("should return crous list", async () => {
 		const crousList = await crousApi.getCrousList();
 		expect(crousList).toHaveLength(26);
+	});
+
+	it("should have a precise number of restaurant for Nantes Crous ", async () => {
+		const crousNantes = await crousApi.getCrous("nantes");
+		expect(crousNantes?.restaurants.list).toHaveLength(56);
+	});
+
+	it("should be able to find a precise restaurant and menus", async () => {
+		jest.useFakeTimers().setSystemTime(new Date("2023-06-09"));
+		const restaurant = await crousApi.getRestaurant("603");
+		expect(restaurant?.nom).toBe("Resto U' Aubépin");
+		expect(restaurant?.paiements).toHaveLength(1);
+		expect(restaurant?.getTodayMenu()).toBeDefined();
 	});
 });
